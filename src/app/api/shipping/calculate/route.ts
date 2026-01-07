3d420d218e907ffb0d0dddc5c3934fd0dca69597
f3multi <f3multi@gmail.com>
2026-01-07 11:20:28 -0300

    fix: remove isPickup property to fix TS error

--- src/app/api/shipping/calculate/route.ts
+++ src/app/api/shipping/calculate/route.ts
@@ -194,7 +194,6 @@ export async function POST(request: NextRequest) {
                 delivery_time: 0,
                 delivery_range: null,
                 freeShipping: true,
-                isPickup: true,
             });
         }
 
